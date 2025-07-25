9067679674f505ea9dda7858b7d6b630ce1b05a9
syedaraheen <raheenbukhari.01@gmail.com>
2025-07-25 17:27:47 +0500

    Add personal introduction to main.py and README.md

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Intro
-# Intro
+
+My name is Raheen Bukhari and I am an Electrical Engineer. I am pursuing an AI Agentic course in AI Skill Bridge.
--- main.py
+++ main.py
@@ -1,1 +1,1 @@
-print("Hello")
+print("My name is Raheen Bukhari and I am an Electrical Engineer. I am pursuing an AI Agentic course in AI Skill Bridge.")
